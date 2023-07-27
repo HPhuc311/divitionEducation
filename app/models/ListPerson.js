805674ff793ff2baa91324f48a4e5b1e930a00c0
HoangPhucPc <hoangphuc311tn@gmail.com>
2023-07-28 00:14:04 +0700

    done

--- app/models/ListPerson.js
+++ app/models/ListPerson.js
@@ -1,1 +1,27 @@
-class ListPerson {}
+class ListP {
+    constructor(){
+        this.empty = []
+    }
+
+    addUser(u){
+        this.empty.push(u)
+    }
+    delUser(i){
+        this.empty = this.empty.filter(f => f.i !== i);
+    }
+    editUser(n,i){
+        const index = this.empty.findIndex(f => f.i === i);
+        this.empty[index] = n;
+    }
+
+    sortByName() {
+        this.empty.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    
+    filterByType(type) {
+        return this.empty.filter(empty => empty instanceof type);
+    }
+
+}
+
+export default ListP
