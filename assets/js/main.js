29d662ca9cae52d5dd843c7dc3484cc962177758
HoangPhuc <hoangphuc311tn@gmail.com>
2023-07-28 07:41:39 +0700

    push

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const getElement = (element) => document.querySelector(element)
+
+getElement('#btnThem').onclick = () => {
+    getElement('#btnCapNhat').style.display = 'onclick'
+}
+
+const getInfor = () => {
+    const element = document.querySelectorAll()
+}
