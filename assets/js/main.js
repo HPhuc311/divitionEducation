a5870952929a9fbfac9bfec1746ba08ef2dde93e
HoangPhuc <hoangphuc311tn@gmail.com>
2023-07-28 12:03:11 +0700

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 const getElement = (element) => document.querySelector(element)
 
 getElement('#btnThem').onclick = () => {
-    getElement('#btnCapNhat').style.display = 'onclick'
+    getElement('#btnCapNhat').style.display = 'none'
+    // lấy thông tin từ form
+    getInfor()
 }
 
 const getInfor = () => {
-    const element = document.querySelectorAll()
+    const element = document.querySelectorAll("#listForm input, #listForm select")
+    console.log("element", element);
 }
