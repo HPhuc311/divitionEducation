60cc709933ec6524319627a72c76affe9c87dd64
HoangPhuc <hoangphuc311tn@gmail.com>
2023-07-29 16:15:53 +0700

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,14 @@ const getChoose = () => {
     l.addEventListener('change' , (event) =>{
         hideAllInputFields()
         const selectedValue = event.target.value;
-        if(selectedValue === "1"){
+        if(selectedValue === "Student"){
             showInputFields('#math')
             showInputFields('#physical')
             showInputFields('#chemistry')
-        }else if(selectedValue === "2"){
+        }else if(selectedValue === "Employee"){
             showInputFields('#numberofDays')
             showInputFields('#dailyofWage')
-        }else if(selectedValue === "3"){
+        }else if(selectedValue === "Customer"){
             showInputFields('#nameofCompany')
             showInputFields('#invoice')
             showInputFields('#evaluate')
@@ -44,13 +44,13 @@ getChoose()
 //  ----------------------------------------------------------------
  getElement('#btnThem').onclick = () => {
     getElement('#btnCapNhat').style.display = 'none'
+    
  }
 
 //  ----------------------------------------------------------------
 
 
 const personList = new ListPerson()
-console.log('personList', personList.personList)
 
 getElement('#btnThemPerson').onclick = () => {
     addUser()
@@ -63,32 +63,54 @@ const addUser = () => {
     const email = getElement('#email').value
     const type = getElement('#loai')
     
-    let newUser;
-    personList.addPerson(newUser);
-
+    let newUser = {};
+    
+    // displayUsers()
+    
     type.addEventListener('change' , (event) =>{
         const selectedValue = event.target.value;
-        if(selectedValue === "1"){
+        console.log("selectedValue", selectedValue);
+        if(selectedValue === "Student"){
+            console.log(123)
            const math = getElement('#input1').value
            const physical = getElement('#input2').value
            const chemistry = getElement('#input3').value
            newUser = new Student(id, name, address, email, math, physical, chemistry)
-        }else if(selectedValue === "2"){
+           console.log("newUser", newUser);
+        }else if(selectedValue === "Employee"){
+            console.log(124)
             const workDays = getElement('#input4').value
             const dailySalary = getElement('#input5').value
             newUser = new Employee(id, name, address, email, workDays, dailySalary)
-        }else if(selectedValue === "3"){
+            console.log("newUser", newUser);
+        }else if(selectedValue === "Customer"){
+            console.log(126)
             const companyName = getElement('#input6').value
             const orderValue = getElement('#input7').value
             const rating = getElement('#input8').value
             newUser = new Customer(id, name, address, email, companyName, orderValue, rating)
+            console.log("newUser", newUser);
         }
     })
-    // displayUsers()
+    personList.addPerson(newUser);
  }
 
 
-//  const displayUsers = (users = personList.personList) => {
-//     const userListElement = getElement("#tbodyList");
-//     userListElement.innerHTML = ""; 
-// }
+ function displayUsers(users = personList.personList) {
+    const userListElement = getElement("#tbodyList");
+    userListElement.innerHTML = "";
+  
+    for (const user of users) {
+      const li = document.createElement("li");
+  
+      if (user instanceof Student) {
+        li.textContent = `Học viên: ${user.name}, Điểm TB: ${user.calculateAverage()}`;
+      } else if (user instanceof Employee) {
+        li.textContent = `Nhân viên: ${user.name}, Lương: ${user.calculateSalary()}`;
+      } else if (user instanceof Customer) {
+        li.textContent = `Khách hàng: ${user.name}, Đánh giá: ${user.rating}`;
+      }
+  
+      userListElement.appendChild(li);
+    }
+  }
